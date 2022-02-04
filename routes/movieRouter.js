535a1848dcdb7597e3b5738be31c70dfddd0b91a
Manelus <94968227+Manelus@users.noreply.github.com>
2022-02-04 18:56:32 +0100

    minos fixes

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -5,31 +5,31 @@ const axios = require('axios');
 
 const app = express();
 
+//Add datos de prueba
+//let movies = axios.get(`https://api.themoviedb.org/3/movie/latest?api_key=cea68b520beecac6718820e4ac576c3alanguage=es-ES`);
+
+//ENDPOINTS CRUD-------------------------------------------------------------------------------
 //Metodo GET - READ ALL
 router.get('/', async (req, res) => {
-    let movies = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`);
-    peliculas = movies.data.results;
-    res.json({message: peliculas});
+    let movies = await axios.get(`https://api.themoviedb.org/3/movie/latest?api_key=cea68b520beecac6718820e4ac576c3alanguage=es-ES`);
+    console.log(movies);
+    res.json(movies);
 });
 
 //Metodo GET - READ por ID
-router.get('/id/:id', async (req, res) => {
+router.get('/:id', (req, res) => {
     //const id = rep.params.id;
-    const idFilm = req.params.id;
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${idFilm}?api_key=cea68b520beecac6718820e4ac576c3a`);
-    film = response.data;
-    res.json(film);
+    const {id} = req.params;
+    let movie = movies.find(movie => movie.id == id);
+    res.json(movies);
 });
 
 //Metodo GET - READ por TITLE
-router.get('/title/:title', async (req, res) => {
+router.get('/:title', (req, res) => {
     //const id = rep.params.id;
-    let response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES`);
-    films = response.data.results;
-    filmsList = films.filter((film) =>
-        film.title.toLowerCase().indexOf(req.params.title.toLowerCase()) !== -1
-    );
-    res.json(filmsList);
+    const {title} = req.params;
+    let movie = movies.find(movie => movie.title == title);
+    res.json(movies);
 });
 
 module.exports = router;
